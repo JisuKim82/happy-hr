f7ec0d3de0df0e5140016ac1d7b2f381c478910c
Jisu Kim <jisu.kim@flatironschool.com>
2014-07-07 14:17:17 -0400

    Add ReviewController

--- app.js
+++ app.js
@@ -15,6 +15,14 @@
     };
   });
 
+  app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
   var drinks = [{ 
       name: 'Reyka Vodka',
       price: 25,
